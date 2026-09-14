fcd62cfc348ef36c38abf3c877b6ff58fbb70329
Fatma Eid <feid7949@gmail.com>
2026-09-14 18:02:10 +0300

    Cmputing booking&stays ...Displaying statitics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import { useRecenetBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecenetStays } from "./useRecentStays";
+import Stats from "./Stats";
+import { UseCabins } from "../cabins/UseCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,9 +13,25 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { isLoading: isLoading1, bookings } = useRecenetBookings();
+  const {
+    isLoading: isLoading2,
+    stays,
+    confirmedStays,
+    numDays,
+  } = useRecenetStays();
+  const { isLoading: isLoading3, cabins } = UseCabins();
+
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+
   return (
     <StyledDashboardLayout>
-      <div>Statitsc</div>
+      <Stats
+        bookings={bookings}
+        confiremedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
       <div>Statitsc1</div>
       <div>Statitsc2</div>
       <div>Statitsc3</div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
 import DashboardFilter from "../features/dashboard/DashboardFilter";
 import DashboardLayout from "../features/dashboard/DashboardLayout";
+import { useRecenetBookings } from "../features/dashboard/useRecentBookings";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Spinner from "../ui/Spinner";
 
 function Dashboard() {
   return (
